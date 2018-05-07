e96ffdf0f600ea96baaff0b260093cbc9cd262b1
zhangqiang116 <zhangqiang12@ZB-PC0DYU58.360buyAD.local>
2018-05-07 21:38:37 +0800

    fix

--- 0heng-web/src/main/java/com/lingheng/web/controller/IndexController.java
+++ 0heng-web/src/main/java/com/lingheng/web/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.lingheng.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
     @GetMapping("/index")
     public String index() {
+        logger.info("logger enable!");
         return "/index.html";
     }
 
