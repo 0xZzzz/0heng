78e85621f82d93565803fb8c4190052f74c9986f
zhangqiang116 <zhangqiang12@ZB-PC0DYU58.360buyAD.local>
2018-05-08 08:55:54 +0800

    delete note

--- 0heng-web/src/main/java/com/lingheng/web/controller/Startup.java
+++ 0heng-web/src/main/java/com/lingheng/web/controller/Startup.java
@@ -17,7 +17,6 @@ public class Startup extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // 注意这里要指向原先用main方法执行的Application启动类
         return builder.sources(this.getClass());
     }
 
