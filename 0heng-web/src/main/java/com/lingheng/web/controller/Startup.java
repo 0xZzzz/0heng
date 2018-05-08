ce9def4ac53de90cb9c8da94c7942a89dbe42c81
zhangqiang116 <zhangqiang12@ZB-PC0DYU58.360buyAD.local>
2018-05-08 08:43:29 +0800

    fix

--- 0heng-web/src/main/java/com/lingheng/web/controller/Startup.java
+++ 0heng-web/src/main/java/com/lingheng/web/controller/Startup.java
@@ -2,15 +2,23 @@ package com.lingheng.web.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Created by 0xZzzz
  */
 @SpringBootApplication
-public class Startup {
+public class Startup extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Startup.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(this.getClass());
     }
 
 }
